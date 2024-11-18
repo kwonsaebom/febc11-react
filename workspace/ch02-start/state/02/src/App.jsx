ffb05ab191b4440a44e3e311c9168c49acf08ca6
Saebom <spring180@naver.com>
2024-11-18 16:12:49 +0900

    form 학습

--- workspace/ch02-start/state/02/src/App.jsx
+++ workspace/ch02-start/state/02/src/App.jsx
@@ -4,18 +4,26 @@ function App() {
   const [number, setNumber] = useState(0);
   return (
     <>
-      <h2>02 이벤트 핸들러에서 State 값을 여러번 변경했을 때 문제점</h2>
+      <h2>02 이벤트 핸들러에서 state 값을 여러번 변경했을 때 문제점</h2>
       <p>{number}</p>
       <button
         onClick={() => {
-          // 한번의 이벤트에서는 state값도 한번만 변화
-          console.log("click start", number);
-          setNumber(number + 1); //0 + 1
-          setNumber(number + 1); // 0 + 1
-          console.log("click finish", number);
+          console.log("클릭 처리 시작", number); // 0
+
+          // 호출되는 즉시 리렌데링이 되지 않고 모아 두었다가 한번에 반영됨(배치)
+          // setNumber(number + 1); // 0 + 1
+          // setNumber(number + 1); // 0 + 1
+          // setNumber(number + 1); // 0 + 1
+
+          // 콜백함수의 리턴값을 저장해 두었다가 다음에 호출되는 콜백함수의 인자로 전달 -> 이렇게 하면 값 계속 변경 & 저장 가능
+          setNumber((num) => num + 1); // 0 + 1
+          setNumber((num) => num + 1); // 1 + 1
+          setNumber((num) => num + 1); // 2 + 1
+
+          console.log("클릭 처리 종료", number); // 0
         }}
       >
-        +2
+        +3
       </button>
     </>
   );
--- workspace/ch02-start/state/04/src/App.jsx
+++ workspace/ch02-start/state/04/src/App.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import EditAddress from "./components/EditAddress";
+
 function App() {
   const [user, setUser] = useState({
     _id: 4,
@@ -25,14 +28,41 @@ function App() {
       ],
     },
   });
+
+  const handleAddressChange = (event) => {
+    const address = user.extra.addressBook.find(
+      (address) => address.id === Number(event.target.name)
+    );
+    address.value = event.target.value;
+    setUser({ ...user });
+  };
+
   return (
     <>
       <h2>04 상태관리 대상이 복합 객체일 경우 불변성 관리</h2>
-      <h2>04 상태관리 대상이 복합 객체일 경우 불변성 관리</h2>
       <p>
-        이메일: <br />
-        전화번호: <br />
+        이메일: {user.email}
+        <br />
+        이름: {user.name}
+        <br />
+        전화번호: {user.phone}
+        <br />
+      </p>
+      <ul>
+        {user.extra.addressBook?.map((address) => (
+          <li key={address.id}>
+            {address.name}: {address.value}
+          </li>
+        ))}
+      </ul>
+      <p>
+        <EditAddress
+          addressBook={user.extra.addressBook}
+          handleAddressChange={handleAddressChange}
+        />
       </p>
     </>
   );
 }
+
+export default App;
